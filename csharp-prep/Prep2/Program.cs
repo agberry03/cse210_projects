8be3a27750f865a9d4a5d613da2b83e0948680e5
Allison Berry <ber21031@byui.edu>
2023-01-04 11:33:08 -0700

    Completed core requirements for Prep2 Assignment.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade number? ");
+        string gradeInput = Console.ReadLine();
+        int gradeNum = int.Parse(gradeInput);
+
+        string letter = "";
+        if (gradeNum >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradeNum >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradeNum >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradeNum >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        
+        Console.WriteLine($"Grade: {letter}");
+        
+        if (gradeNum >= 70)
+        {
+            Console.WriteLine("You Passed! Good job!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass...You'll get it next time!");
+        }
     }
 }
