508602a2174079cc3665b3ef6ef7cbf075eb0915
Allison Berry <ber21031@byui.edu>
2023-01-07 08:31:15 -0700

    Completed the 3 Core Challenges and the Stretch Challenges.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1, 100);
+
+        int guessNum = -1;
+
+        List<int> guessList = new List<int>() {};
+        string answer = "yes";
+    
+        while (answer.ToLower() == "yes")
+        {
+            while (guessNum != magicNum)
+            {
+                Console.Write("What is your guess? ");
+                guessNum = int.Parse(Console.ReadLine());
+                
+                if (guessNum < magicNum)
+                {
+                    Console.WriteLine("Higher");
+                    guessList.Add(guessNum);
+                }
+                else if (guessNum > magicNum)
+                {
+                    Console.WriteLine("Lower");
+                    guessList.Add(guessNum);
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    guessList.Add(guessNum);
+                    Console.WriteLine($"You guessed it in {guessList.Count()} tries.");
+                }
+            }
+            Console.Write("Would you like to play again? ");
+            answer = Console.ReadLine();
+
+            guessList.Clear();
+            magicNum = randomGenerator.Next(1, 100);
+        }
     }
 }
