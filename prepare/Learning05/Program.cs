02fc5c26cf85a5f706305cc936bdb2ce735d51db
Allison Berry <ber21031@byui.edu>
2023-02-25 12:00:35 -0700

    Completed the Learning Activity.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square1 = new Square("black", 3);
+        Rectangle rectangle1 = new Rectangle("purple", 4, 3);
+        Circle circle1 = new Circle("blue", 2);
+
+        /* Test the shape child classes.
+        Console.WriteLine($"Shape color: {square1.GetColor()} | Shape area: {square1.GetArea()}");
+        Console.WriteLine($"Shape color: {rectangle1.GetColor()} | Shape area: {rectangle1.GetArea()}");
+        Console.WriteLine($"Shape color: {circle1.GetColor()} | Shape area: {circle1.GetArea()}");
+        */
+
+        List<Shape> shapes = new List<Shape>{square1, rectangle1, circle1};
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape color: {shape.GetColor()} | Shape area: {shape.GetArea()}");
+        }
     }
 }
